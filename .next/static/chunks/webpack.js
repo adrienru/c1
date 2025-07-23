2748b5e1f56d3bdf1ef469ee4052a9d3f6e43c83
adrienru <u25253967@utp.edu.pe>
2025-07-23 00:05:45 -0500

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50facee83cca7200"; }
+/******/ 		__webpack_require__.h = function() { return "2acebba3d15132e7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
